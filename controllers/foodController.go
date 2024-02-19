cee1062e1f4029c95ede98941c69aa4413bdd2c5
A7medAbdien <ahmed.g.abdien@gmail.com>
2024-02-19 12:04:46 +0300

    /controller - init

---
+++ controllers/foodController.go
@@ -0,0 +1,22 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func GetFoods() gin.HandlerFunc {
+	return func(ctx *gin.Context) {}
+}
+
+func GetFood() gin.HandlerFunc {
+	return func(ctx *gin.Context) {}
+}
+
+func CreateFood() gin.HandlerFunc {
+	return func(ctx *gin.Context) {}
+}
+
+func UpdateFood() gin.HandlerFunc {
+	return func(ctx *gin.Context) {}
+}
+
+func round(num float64)                          {}
+func toFixed(num float64, precision int) float64 {}
