3c35bad691aac8a021cd81faaf6f14355113f15d
A7medAbdien <ahmed.g.abdien@gmail.com>
2024-02-21 01:19:04 +0300

    foodController - done

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -3,10 +3,12 @@ package controller
 import (
 	"context"
 	"fmt"
+	"log"
 	"math"
 	"net/http"
 	"restaurant-management-backend/database"
 	"restaurant-management-backend/models"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -14,13 +16,69 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 var validate = validator.New()
 
 func GetFoods() gin.HandlerFunc {
-	return func(ctx *gin.Context) {}
+	return func(c *gin.Context) {
+		// Create a context with a timeout
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		// Parse and handle query parameters for pagination
+		recordPerPage, err := strconv.Atoi(c.Query("recordPerPage"))
+		if err != nil || recordPerPage < 1 {
+			recordPerPage = 10
+		}
+
+		page, err := strconv.Atoi(c.Query("page"))
+		if err != nil || page < 1 {
+			page = 1
+		}
+
+		startIndex := (page - 1) * recordPerPage
+		if startIndex < 0 {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Invalid startIndex value"})
+			return
+		}
+
+		// MongoDB aggregation pipeline stages
+		matchStage := bson.D{{"$match", bson.D{{}}}}
+		groupStage := bson.D{{"$group", bson.D{{"_id", bson.D{{"_id", "null"}}}, {"total_count", bson.D{{"$sum", 1}}}, {"data", bson.D{{"$push", "$$ROOT"}}}}}}
+		projectStage := bson.D{
+			{
+				"$project", bson.D{
+					{"_id", 0},
+					{"total_count", 1},
+					{"food_items", bson.D{{"$slice", []interface{}{"$data", startIndex, recordPerPage}}}},
+				},
+			},
+		}
+
+		// Perform MongoDB aggregation using the defined pipeline
+		result, err := foodCollection.Aggregate(ctx, mongo.Pipeline{
+			matchStage, groupStage, projectStage,
+		})
+		defer cancel()
+
+		// Handle errors during the MongoDB aggregation
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while listing food items"})
+			return
+		}
+
+		// Decode the result into a slice of BSON documents
+		var allFoods []bson.M
+		if err = result.All(ctx, &allFoods); err != nil {
+			log.Fatal(err)
+		}
+
+		// Respond with the paginated list of food items
+		c.JSON(http.StatusOK, allFoods[0])
+	}
 }
 
 func GetFood() gin.HandlerFunc {
@@ -29,7 +87,7 @@ func GetFood() gin.HandlerFunc {
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 		defer cancel()
 
-		// retrieve food by ID
+		// retrieve and decode
 		foodId := c.Param("food_id")
 		var food models.Food
 
@@ -91,7 +149,83 @@ func CreateFood() gin.HandlerFunc {
 }
 
 func UpdateFood() gin.HandlerFunc {
-	return func(ctx *gin.Context) {}
+	return func(c *gin.Context) {
+		// Create a context with a timeout
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		// Initialize variables for Menu and Food models
+		var menu models.Menu
+		var food models.Food
+
+		// Extract the food ID from the request parameters
+		foodId := c.Param("food_id")
+
+		// Bind JSON data from the request into a Food struct
+		if err := c.BindJSON(&food); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Initialize update object for MongoDB update
+		var updateObj primitive.D
+
+		// Check and update fields in the update object based on the provided JSON data
+		if food.Name != nil {
+			updateObj = append(updateObj, bson.E{"name", food.Name})
+		}
+
+		if food.Price != nil {
+			updateObj = append(updateObj, bson.E{"price", food.Price})
+		}
+
+		if food.Food_image != nil {
+			updateObj = append(updateObj, bson.E{"food_image", food.Food_image})
+		}
+
+		if food.Menu_id != nil {
+			// If Menu ID is provided, check if the menu exists
+			err := menuCollection.FindOne(ctx, bson.M{"menu_id": food.Menu_id}).Decode(&menu)
+			defer cancel()
+			if err != nil {
+				msg := fmt.Sprintf("Menu was not found")
+				c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+				return
+			}
+			updateObj = append(updateObj, bson.E{"menu", food.Menu_id})
+		}
+
+		// Update the "updated_at" field with the current time
+		food.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		updateObj = append(updateObj, bson.E{"updated_at", food.Updated_at})
+
+		// Set up options for the UpdateOne operation, including upsert
+		upsert := true
+		filter := bson.M{"food_id": foodId}
+		opt := options.UpdateOptions{
+			Upsert: &upsert,
+		}
+
+		// Perform the update operation on the MongoDB collection
+		result, err := foodCollection.UpdateOne(
+			ctx,
+			filter,
+			bson.D{
+				{Key: "$set", Value: updateObj},
+			},
+			&opt,
+		)
+
+		// Handle errors during the update operation
+		if err != nil {
+			msg := fmt.Sprintf("Food item update failed")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		// Respond with the result of the update operation
+		c.JSON(http.StatusOK, result)
+	}
 }
 
 func round(num float64) int {
